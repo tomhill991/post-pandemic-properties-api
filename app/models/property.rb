b94a322dbd6e278c244379727ecd31e57fa805ef
tomhill991 <tom@mediasia-interactive.com>
2021-05-04 11:31:41 +0800

    added property validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,9 @@
 class Property < ApplicationRecord
+    belongs_to :user
+    has_many :reviews, through: :bookings
     has_many_attached :images
+    validates :max_guests, presence: true
+    validates :title, length: { maximum: 100 }, allow_blank: false, presence: true
+    validates :description, length: { maximum: 250 }, allow_blank: false, presence: true
+    validates :address, presence: true
 end
