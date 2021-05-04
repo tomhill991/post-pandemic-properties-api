6c6cd7330061a7c75b4580719cccc0f7e6a70adb
tomhill991 <tom@mediasia-interactive.com>
2021-05-04 11:04:41 +0800

    created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_030305) do
+ActiveRecord::Schema.define(version: 2021_05_04_030411) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "property_id", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_05_04_030305) do
     t.string "address"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
