4db042c169e4ce2f7e204e1e3fee54393b97f992
tomhill991 <tom@mediasia-interactive.com>
2021-05-04 11:36:39 +0800

    added review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+    belongs_to :bookings
+    validates :description, presence: true
+    validates :ratings, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
