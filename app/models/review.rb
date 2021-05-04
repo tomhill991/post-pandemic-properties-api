ea2efd4f37d64949827a6e6dc429698c7accefe0
tomhill991 <tom@mediasia-interactive.com>
2021-05-04 12:11:15 +0800

    validates ratings in review >0 =< 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :bookings
     validates :description, presence: true
-    validates :ratings, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+    validates :ratings, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
